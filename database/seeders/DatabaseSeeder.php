55ba2f6efaa0b31777f0d3a7ae1da2061e08b0c9
Rainfog <muhammadzhafranbahij@gmail.com>
2024-11-30 10:46:08 +0700

    feat (auth): Using authentication with Laravel UI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::create([
+            'name' => 'kirakira4141',
+            'email' => 'kirakira4141@gmail.com',
+            'password' => 'kirakira4141',
         ]);
     }
 }
